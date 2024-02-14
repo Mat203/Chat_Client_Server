1d53ea94d7eaefada74e439b53e98ab288774502
Mat_Tol <matvey.talalaevskiy@gmail.com>
2024-02-14 21:42:05 +0200

    More structurised code

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -67,77 +67,69 @@ public:
 	}
 };
 
-void receiveMessages(SOCKET clientSocket) {
-	char buffer[4096];
-	while (true) {
-		int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-		if (bytesReceived <= 0) {
-			std::cerr << "Server disconnected.\n";
-			break;
-		}
-		buffer[bytesReceived] = '\0';
-		std::string message(buffer);
-		if (message.rfind("Client ", 0) == 0 && message.find(" wants to send a file. Type /receivefile to accept.") != std::string::npos) {
-			std::cout << message << std::endl;
-			std::cout << "Do you want to receive the file? (yes/no): ";
-			std::string answer;
-			std::getline(std::cin, answer);
-			if (answer == "yes") {
-				send(clientSocket, "/receivefile", 12, 0);
-				FileHandler::receiveFile(clientSocket, "Client_" + std::to_string(clientSocket));
-			}
-		}
-		else {
-			std::cout << buffer << std::endl;
-		}
-	}
-}
+class ChatClient {
+private:
+    SOCKET clientSocket;
+
+public:
+    void receiveMessages() {
+        char buffer[4096];
+        while (true) {
+            int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (bytesReceived <= 0) {
+                std::cerr << "Server disconnected.\n";
+                break;
+            }
+            buffer[bytesReceived] = '\0';
+            std::cout << buffer << std::endl;
+        }
+    }
+
+    void start() {
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            std::cerr << "WSAStartup failed.\n";
+            return;
+        }
+        clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cerr << "Socket creation failed.\n";
+            WSACleanup();
+            return;
+        }
+        sockaddr_in serverAddr;
+        serverAddr.sin_family = AF_INET;
+        InetPton(AF_INET, L"127.0.0.1", &serverAddr.sin_addr);
+        serverAddr.sin_port = htons(8080);
+        if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) ==
+            SOCKET_ERROR) {
+            std::cerr << "Connection failed.\n";
+            closesocket(clientSocket);
+            WSACleanup();
+            return;
+        }
+        std::cout << "Connected to server.\n";
+        std::cout << "Enter room ID: ";
+        std::string roomId;
+        std::getline(std::cin, roomId);
+        send(clientSocket, roomId.c_str(), roomId.size() + 1, 0);
+        // Start a thread to receive messages from the server
+        std::thread receiveThread(&ChatClient::receiveMessages, this);
+        receiveThread.detach();
+        // Main thread to send messages to the server
+        std::string message;
+        while (true) {
+            std::getline(std::cin, message);
+            send(clientSocket, message.c_str(), message.size() + 1, 0);
+        }
+        // Close the socket and clean up
+        closesocket(clientSocket);
+        WSACleanup();
+    }
+};
 
 int main() {
-	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		std::cerr << "WSAStartup failed.\n";
-		return 1;
-	}
-	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (clientSocket == INVALID_SOCKET) {
-		std::cerr << "Socket creation failed.\n";
-		WSACleanup();
-		return 1;
-	}
-	sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	InetPton(AF_INET, L"127.0.0.1", &serverAddr.sin_addr);
-	serverAddr.sin_port = htons(8080);
-	if (connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) ==
-		SOCKET_ERROR) {
-		std::cerr << "Connection failed.\n";
-		closesocket(clientSocket);
-		WSACleanup();
-		return 1;
-	}
-	std::cout << "Connected to server.\n";
-	std::cout << "Enter room ID: ";
-	std::string roomId;
-	std::getline(std::cin, roomId);
-	send(clientSocket, roomId.c_str(), roomId.size() + 1, 0);
-	// Start a thread to receive messages from the server
-	std::thread receiveThread(receiveMessages, clientSocket);
-	// Main thread to send messages to the server
-	std::string message;
-	while (true) {
-		std::getline(std::cin, message);
-		if (message.rfind("/sendfile ", 0) == 0) {
-			std::string fileName = message.substr(10);
-			send(clientSocket, message.c_str(), message.size() + 1, 0);
-			FileHandler::sendFile(clientSocket, "Client_" + std::to_string(clientSocket), fileName.c_str());
-		}
-		else {
-			send(clientSocket, message.c_str(), message.size() + 1, 0);
-		}
-	}
-	// Close the socket and clean up
-	closesocket(clientSocket);
-	WSACleanup();
-	return 0;
+    ChatClient client;
+    client.start();
+    return 0;
 }
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -72,121 +72,119 @@ public:
 	}
 };
 
-void broadcastMessage(const std::string& message, SOCKET senderSocket, const std::string& roomId) {
-	std::lock_guard<std::mutex> lock(consoleMutex);
-	std::string fullMessage = "Client " + std::to_string(senderSocket) + ": " + message;
-	std::cout << fullMessage << std::endl;
-	for (SOCKET client : rooms[roomId]) {
-		if (client != senderSocket) {
-			send(client, fullMessage.c_str(), fullMessage.size() + 1, 0);
-		}
-	}
-}
+class ChatServer {
+private:
+    std::map<std::string, std::vector<SOCKET>> rooms;
+    std::mutex consoleMutex;
 
+public:
+    void broadcastMessage(const std::string& message, SOCKET senderSocket, const std::string& roomId) {
+        std::lock_guard<std::mutex> lock(consoleMutex);
+        std::string fullMessage = "Client " + std::to_string(senderSocket) + ": " + message;
+        std::cout << fullMessage << std::endl;
+        for (SOCKET client : rooms[roomId]) {
+            if (client != senderSocket) {
+                send(client, fullMessage.c_str(), fullMessage.size() + 1, 0);
+            }
+        }
+    }
 
-void handleClient(SOCKET clientSocket) {
-	char buffer[4096];
-	int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-	if (bytesReceived <= 0) {
-		std::cerr << "Failed to get room ID from client.\n";
-		return;
-	}
-	buffer[bytesReceived] = '\0';
-	std::string roomId(buffer);
+    void handleClient(SOCKET clientSocket) {
+        char buffer[4096];
+        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesReceived <= 0) {
+            std::cerr << "Failed to get room ID from client.\n";
+            return;
+        }
+        buffer[bytesReceived] = '\0';
+        std::string roomId(buffer);
 
-	rooms[roomId].push_back(clientSocket);
+        rooms[roomId].push_back(clientSocket);
 
-	std::string joinMessage = "joined the room.";
-	broadcastMessage(joinMessage, clientSocket, roomId);
+        std::string joinMessage = "joined the room.";
+        broadcastMessage(joinMessage, clientSocket, roomId);
 
-	std::string dirName = "Client_" + std::to_string(clientSocket);
-	_mkdir(dirName.c_str());
+        std::string dirName = "Client_" + std::to_string(clientSocket);
+        _mkdir(dirName.c_str());
 
-	while (true) {
-		bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-		if (bytesReceived <= 0) {
-			std::lock_guard<std::mutex> lock(consoleMutex);
-			std::cout << "Client " << clientSocket << " disconnected.\n";
-			break;
-		}
-		buffer[bytesReceived] = '\0';
-		std::string message(buffer);
+        while (true) {
+            bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (bytesReceived <= 0) {
+                std::lock_guard<std::mutex> lock(consoleMutex);
+                std::cout << "Client " << clientSocket << " disconnected.\n";
+                break;
+            }
+            buffer[bytesReceived] = '\0';
+            std::string message(buffer);
 
-		if (message.rfind("/sendfile ", 0) == 0) {
-			std::string fileName = message.substr(10); 
+            if (message.rfind("/rejoin ", 0) == 0) {
+                std::string newRoomId = message.substr(8);
 
-			FileHandler::receiveFile(clientSocket, dirName);
+                rooms[roomId].erase(std::remove(rooms[roomId].begin(), rooms[roomId].end(), clientSocket), rooms[roomId].end());
 
-			std::string fileMessage = "Client " + std::to_string(clientSocket) + " wants to send a file. Type /receivefile to accept.";
-			broadcastMessage(fileMessage, clientSocket, roomId);
-		}
-		else if (message.rfind("/receivefile", 0) == 0) {
-			std::cout << "Client egssfbsb disconnected.\n";
-			FileHandler::sendFile(clientSocket, dirName.c_str(), "received_file.txt");
-		}
-		else if (message.rfind("/rejoin ", 0) == 0) {
-			std::string newRoomId = message.substr(8);
+                roomId = newRoomId;
+                rooms[roomId].push_back(clientSocket);
 
-			rooms[roomId].erase(std::remove(rooms[roomId].begin(), rooms[roomId].end(), clientSocket), rooms[roomId].end());
+                joinMessage = "Client " + std::to_string(clientSocket) + " joined the room.";
+                broadcastMessage(joinMessage, clientSocket, roomId);
+            }
+            else {
+                broadcastMessage(message, clientSocket, roomId);
+            }
+        }
+        closesocket(clientSocket);
+    }
 
-			roomId = newRoomId;
-			rooms[roomId].push_back(clientSocket);
-
-			joinMessage = "Client " + std::to_string(clientSocket) + " joined the room.";
-			broadcastMessage(joinMessage, clientSocket, roomId);
-		}
-		else {
-			broadcastMessage(message, clientSocket, roomId);
-		}
-	}
-	closesocket(clientSocket);
-}
-
+    void start() {
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            std::cerr << "WSAStartup failed.\n";
+            return;
+        }
+        SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (serverSocket == INVALID_SOCKET) {
+            std::cerr << "Socket creation failed.\n";
+            WSACleanup();
+            return;
+        }
+        sockaddr_in serverAddr;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_port = htons(8080);
+        if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) ==
+            SOCKET_ERROR) {
+            std::cerr << "Bind failed.\n";
+            closesocket(serverSocket);
+            WSACleanup();
+            return;
+        }
+        if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+            std::cerr << "Listen failed.\n";
+            closesocket(serverSocket);
+            WSACleanup();
+            return;
+        }
+        std::cout << "Server is listening on port 8080...\n";
+        while (true) {
+            SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+            if (clientSocket == INVALID_SOCKET) {
+                std::cerr << "Accept failed.\n";
+                closesocket(serverSocket);
+                WSACleanup();
+                return;
+            }
+            std::lock_guard<std::mutex> lock(consoleMutex);
+            std::cout << "Client " << clientSocket << " connected.\n";
+            std::thread clientThread(&ChatServer::handleClient, this, clientSocket);
+            clientThread.detach(); // Detach the thread to allow handling multiple clients concurrently
+        }
+        closesocket(serverSocket);
+        WSACleanup();
+    }
+};
 
 int main() {
-	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		std::cerr << "WSAStartup failed.\n";
-		return 1;
-	}
-	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (serverSocket == INVALID_SOCKET) {
-		std::cerr << "Socket creation failed.\n";
-		WSACleanup();
-		return 1;
-	}
-	sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = INADDR_ANY;
-	serverAddr.sin_port = htons(8080);
-	if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) ==
-		SOCKET_ERROR) {
-		std::cerr << "Bind failed.\n";
-		closesocket(serverSocket);
-		WSACleanup();
-		return 1;
-	}
-	if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
-		std::cerr << "Listen failed.\n";
-		closesocket(serverSocket);
-		WSACleanup();
-		return 1;
-	}
-	std::cout << "Server is listening on port 8080...\n";
-	while (true) {
-		SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
-		if (clientSocket == INVALID_SOCKET) {
-			std::cerr << "Accept failed.\n";
-			closesocket(serverSocket);
-			WSACleanup();
-			return 1;
-		}
-		std::lock_guard<std::mutex> lock(consoleMutex);
-		std::cout << "Client " << clientSocket << " connected.\n";
-		std::thread clientThread(handleClient, clientSocket);
-		clientThread.detach(); // Detach the thread to allow handling multiple clients concurrently
-	}
-	closesocket(serverSocket);
-	WSACleanup();
-	return 0;
+    ChatServer server;
+    server.start();
+    return 0;
 }
